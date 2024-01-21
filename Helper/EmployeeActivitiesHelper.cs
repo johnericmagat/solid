73f19c700fe08663807a8952f85ccfb2392f253e
John Eric Magat <johnericmagat@gmail.com>
2024-01-22 00:15:37 +0800

    Liskov Substitution Principle (LSP)

---
+++ Helper/EmployeeActivitiesHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid.Helper
+{
+	public interface EmployeeActivitiesHelper
+	{
+		public void DTR();
+		public void OvertimeApplication();
+		public void LeaveApplication();
+		public void ChangeShift();
+		public void PayrollCheck();
+		public void LoanApplication();
+	}
+}
---
+++ View/EmployeePortalView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using solid.Helper;
+
+namespace solid.View
+{
+	public class EmployeePortalView : EmployeeActivitiesHelper
+	{
+		public void DTR()
+		{
+			// some logic
+		}
+		public void OvertimeApplication()
+		{
+			// some logic
+		}
+		public void LeaveApplication()
+		{
+			// some logic
+		}
+		public void ChangeShift()
+		{
+			// some logic
+		}
+		public void PayrollCheck()
+		{
+			// some logic
+		}
+		public void LoanApplication()
+		{
+			// some logic
+		}
+	}
+}
