a620b1577187a2d056666b2ca56e60d2051390dc
John Eric Magat <johnericmagat@gmail.com>
2024-01-21 23:02:46 +0800

    Open–Closed Principle (OCP)

---
+++ Controller/EmployeeDailyRateController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using solid.Helper;
+
+namespace solid.Controller
+{
+	public class EmployeeDailyRateController : EmployeeRateHelper
+	{
+		decimal MonthlySalary;
+		int NoOfWorkDays;
+
+		public decimal Compute()
+		{
+			return MonthlySalary / NoOfWorkDays;
+		}
+	}
+}
---
+++ Controller/EmployeeHourlyRateController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using solid.Helper;
+
+namespace solid.Controller
+{
+	public class EmployeeHourlyRateController : EmployeeRateHelper
+	{
+		decimal MonthlySalary;
+		int NoOfWorkDays;
+		int NoOfHours;
+
+		public decimal Compute()
+		{
+			return (MonthlySalary / NoOfWorkDays) / NoOfHours;
+		}
+	}
+}
