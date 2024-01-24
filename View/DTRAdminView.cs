6ae3c40641a7a69981618c7ff51c22ba68e48154
John Eric Magat <johnericmagat@gmail.com>
2024-01-24 10:30:35 +0800

    Interface Segregation Principle (ISP)

---
+++ View/DTRAdminView.cs
@@ -0,0 +1,22 @@
+﻿using solid.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid.View
+{
+	public class  DTRAdminView : CalculateDTRHelper
+	{
+		public void Calculate()
+		{
+			// DTR Admin has started calculating DTR.
+		}
+
+		public void DTR()
+		{
+			// DTR Admin is calculating the employees DTR.
+		}
+	}
+}
---
+++ View/PayrollAdminView.cs
@@ -0,0 +1,22 @@
+﻿using solid.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid.View
+{
+	public class PayrollAdminView : CalculatePayrollHelper
+	{
+		public void Calculate()
+		{
+			// Payroll Admin has started calculating Payroll.
+		}
+
+		public void Payroll()
+		{
+			// Payroll Admin is calculating the employees Payroll.
+		}
+	}
+}
