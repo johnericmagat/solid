9814012a921276c1fb6aad57afd0488d9555d018
John Eric Magat <johnericmagat@gmail.com>
2024-01-25 09:55:33 +0800

    Dependency Inversion Principle (DIP)

---
+++ Controller/CompanyController.cs
@@ -0,0 +1,17 @@
+﻿using solid.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid.Controller
+{
+	public class CompanyController : BankHelper
+	{
+		public void LoanMoney()
+		{
+			// some logic
+		}
+	}
+}
---
+++ View/EmployeeView.cs
@@ -0,0 +1,17 @@
+﻿using solid.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid.View
+{
+	public class EmployeeView : BankHelper
+	{
+		public void LoanMoney()
+		{
+			// some logic
+		}
+	}
+}
